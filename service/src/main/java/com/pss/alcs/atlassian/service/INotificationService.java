4b634f3cf8fa3b6b6281f05414b10b2870da3c53
karanamsubbarao <karanamsubbarao@gmail.com>
2015-04-04 12:51:41 +0530

    Initial Commit

---
+++ service/src/main/java/com/pss/alcs/atlassian/service/INotificationService.java
@@ -0,0 +1,12 @@
+package com.pss.alcs.atlassian.service;
+
+/**
+ * Created by skaranam on 4/4/2015.
+ */
+public interface INotificationService {
+
+    public void notifyForLeaseRequest();
+
+    public void sendEmail();
+}
+
---
+++ service/src/main/java/com/pss/alcs/atlassian/service/NotificationService.java
@@ -0,0 +1,18 @@
+package com.pss.alcs.atlassian.service;
+
+/**
+ * Created by skaranam on 4/4/2015.
+ */
+public class NotificationService {
+
+    public void notifyForLeaseRequest()
+    {
+
+    }
+
+    public void sendEmail()
+    {
+
+    }
+
+}
