86d60a96f88ca0905beb5fb2b51ad6d284d94155
karanamsubbarao <karanamsubbarao@gmail.com>
2015-04-25 19:43:09 +0530

    ALCS- Initial Commit

---
+++ webapp/src/main/java/com/pss/alcs/atlassian/web/ALCSConfiguration.java
@@ -0,0 +1,34 @@
+package com.pss.alcs.atlassian.web;
+
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by skaranam on 3/16/2015.
+ */
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class ALCSConfiguration extends WebMvcConfigurerAdapter  {
+
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("WEB-INF/*");
+        return resolver;
+    }
+
+
+}
